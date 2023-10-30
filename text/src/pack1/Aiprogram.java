26b3b8ec5b85cd0a8e896b59aae70a4d009424b7
Venkatrao <ongurivenkatrao987@gmail.com>
2023-10-30 12:04:14 +0530

    thirdcommit

--- text/src/pack1/Aiprogram.java
+++ text/src/pack1/Aiprogram.java
@@ -1,9 +1,17 @@
 package pack1;
 
+import java.util.Scanner;
+
 public class Aiprogram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		for(int i=1;i<str.length();i++)
+		{
+			//String s=str[0];
+		}
 
 	}
 
