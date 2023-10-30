5cb40ef662d58324fd049e803b10c9bea7a1061f
Venkatrao <ongurivenkatrao987@gmail.com>
2023-10-30 11:57:14 +0530

    secondcommit

--- text/src/pack1/Welcome.java
+++ text/src/pack1/Welcome.java
@@ -5,6 +5,10 @@ public class Welcome {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("welcome to gprec");
+		for(int i=0;i<10;i++)
+		{
+			System.out.println(i);
+		}
 
 	}
 
